f7de4d1ded3e76b403bedba4f23bf291dd3d5d84
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-05-09 12:46:36 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryListItem = galleryItems
+    .map(
+        (item) =>
+            `<li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                </a>
+            </li>`
+    )
+    .join("");
+
+galleryList.insertAdjacentHTML('beforeend', galleryListItem);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+    captionPosition: 'bottom'
+});
