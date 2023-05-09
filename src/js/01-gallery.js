ce45d23154b586ee4778f9c62fca45146b0fd264
Igor Kushnir <mr.kushnir.igor@gmail.com>
2023-05-09 15:43:42 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -12,27 +12,20 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
+const galleryNextList = document.querySelector(".gallery");
 
-const galleryList = document.querySelector(".gallery");
-
-
-const galleryListItem = galleryItems
+const galleryNextItem = galleryItems
     .map(
         (item) =>
             `<li class="gallery__item">
                 <a class="gallery__link" href="${item.original}">
-                    <img
-                    class="gallery__image"
-                    src="${item.preview}"
-                    data-source="${item.original}"
-                    alt="${item.description}"
-                    />
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
                 </a>
             </li>`
     )
     .join("");
 
-galleryList.insertAdjacentHTML('beforeend', galleryListItem);
+galleryNextList.insertAdjacentHTML('beforeend', galleryNextItem);
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
